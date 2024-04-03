965062b516efe1895149145c98ebb80ea5b29ed5
lxj <1176891456@qq.com>
2024-04-03 15:02:35 +0800

    init

---
+++ theme.config.tsx
@@ -0,0 +1,21 @@
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <div>Codedoc</div>,
+  project: {
+    link: 'https://github.com/Levix0501/code-doc',
+  },
+  docsRepositoryBase: 'https://github.com/Levix0501/code-doc/tree/master',
+  head: function useHead() {
+    const { title } = useConfig();
+
+    return (
+      <>
+        <title>{title}</title>
+      </>
+    );
+  },
+  sidebar: { defaultMenuCollapseLevel: 1, toggleButton: true },
+};
+
+export default config;
