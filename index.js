7ca345068ad25c3d7060e741234e588eec54fb01
lxj <1176891456@qq.com>
2024-04-23 19:52:08 +0800

    feat:require

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log(module);
+console.log(require);
+console.log(exports);
+console.log(module.exports === exports);
---
+++ util.js
@@ -0,0 +1,3 @@
+const getSum = (a, b) => a + b;
+
+exports.getSum = getSum;
