4745fac66850a8dce79f227c2dd2a7c624532000
lxj <1176891456@qq.com>
2024-04-29 16:39:33 +0800

    update

--- leetcode.js
+++ leetcode.js
@@ -1,37 +1,31 @@
-// Definition for a Node.
-function Node(val, left, right, next) {
-  this.val = val === undefined ? null : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-  this.next = next === undefined ? null : next;
-}
-
-const root = new Node(
-  1,
-  new Node(2, new Node(4), new Node(5)),
-  new Node(3, new Node(6), new Node(7))
-);
-
 /**
- * @param {Node} root
- * @return {Node}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-var connect = function (root) {
-  if (!root) return root;
-  let node = root;
-  let next = node.left;
-  while (node && next) {
-    node.left.next = node.right;
-    if (node.next) {
-      node.right.next = node.next.left;
-      node = node.next;
-    } else {
-      node = next;
-      next = node.left;
-    }
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  const queue1 = [p];
+  const queue2 = [q];
+
+  while (queue1.length && queue2.length) {
+    const n1 = queue1.shift();
+    const n2 = queue2.shift();
+    if (!n1 && !n2) continue;
+    if (!n1 || !n2) return false;
+    if (n1.val !== n2.val) return false;
+    queue1.push(n1.left);
+    queue1.push(n1.right);
+    queue2.push(n2.left);
+    queue2.push(n2.right);
   }
 
-  return root;
+  return queue1.length === 0 && queue2.length === 0;
 };
-
-connect(root);
