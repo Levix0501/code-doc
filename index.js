38335c196984d07e5a1717294440805c0c025369
lxj <1176891456@qq.com>
2024-05-09 09:10:17 +0800

    update

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-import { a } from './test.js';
+function myNew(constructor, ...args) {
+  const newInstance = Object.create(constructor.prototype);
+  const ret = constructor.call(newInstance, ...args);
+  return typeof ret === 'object' ? ret : newInstance;
+}
+
+function A(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const a = myNew(A, '小明', 18);
 console.log(a);
--- leetcode.js
+++ leetcode.js
@@ -1,39 +1,30 @@
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-}
-
 /**
- * @param {TreeNode} root
- * @return {number[]}
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
  */
 
-const root = new TreeNode(1, undefined, new TreeNode(2, new TreeNode(3)));
-
-var preorderTraversal = function (root) {
-  if (!root) return [];
-  const ret = [];
-  const stack = [root];
-  while (stack.length > 0) {
-    let node = stack[stack.length - 1];
-    while (node) {
-      ret.push(node.val);
-      if (node.left) {
-        stack.push(node.left);
-      }
-      node = node.left;
-    }
-
-    while (stack.length > 0) {
-      node = stack.pop();
-      if (node.right) {
-        stack.push(node.right);
-        break;
-      }
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  if (!root) return root;
+  let node = root;
+  let next = node.left;
+  while (node && next) {
+    node.left.next = node.right;
+    if (node.next) {
+      node.right.next = node.next.left;
+      node = node.next;
+    } else {
+      node = next;
+      next = node.left;
     }
   }
-  return ret;
+  return root;
 };
-
-preorderTraversal(root);
