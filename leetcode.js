3dbd032455fbf55e8ba0a6e5198fd8ddc4d3728d
lxj <1176891456@qq.com>
2024-05-10 14:17:59 +0800

    update

--- leetcode.js
+++ leetcode.js
@@ -1,27 +1,45 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
  * }
  */
 /**
  * @param {TreeNode} root
- * @return {number[]}
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
  */
-var preorderTraversal = function (root) {
-  const ret = [];
-  const stack = [];
-  let node = root;
-  while (node || stack.length > 0) {
-    while (node) {
-      ret.push(node.val);
-      stack.push(node);
-      node = node.left;
+var lowestCommonAncestor = function (root, p, q) {
+  const parentMap = new Map();
+
+  const dfs = (node) => {
+    if (!node) return false;
+    if (node.left) {
+      parentMap.set(node.left.val, node);
     }
-    node = stack.pop();
-    node = node.right;
+    if (node.right) {
+      parentMap.set(node.right.val, node);
+    }
+    dfs(node.left);
+    dfs(node.right);
+  };
+  dfs(root);
+
+  const set = new Set();
+  let node = p;
+  while (node) {
+    set.add(node);
+    node = parentMap.get(node.val);
   }
+  node = q;
+  while (node) {
+    if (set.has(node)) {
+      return node;
+    }
+    node = parentMap.get(node.val);
+  }
+
   return ret;
 };
